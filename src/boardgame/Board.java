d39d73f14f33f62fbedbcbdd6448296bc937fd9b
matheus <silvamatheusaugusto36@gmail.com>
2020-11-25 11:57:36 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -48,6 +48,19 @@ public class Board {
 	// logica permite que eu acesse uma casa do tabuleiro e que eu
 	// mova uma peça para essa casa, atraves do "position".
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	private boolean positionExists(int row, int column) {
 		return row >=0 && row< rows && column >=0 && column < columns;
 	}
@@ -56,7 +69,7 @@ public class Board {
 		return positionExists(position.getRow(), position.getColumn());
 	}
 	
-	private boolean thereIsAPiece(Position position) {
+	public boolean thereIsAPiece(Position position) {
 		if(!positionExists(position)) {
 			throw new BoardException("position not on the board");
 		}
